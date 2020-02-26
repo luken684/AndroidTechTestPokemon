dd44a302887779a929416449f0fc838d81a196f5
luken684 <luken684@gmail.com>
2020-02-26 20:17:42 +0000

    winning card working to raRE

---
+++ app/src/main/java/io/deepmatter/pokemon/core/RoundFactoryImpl.kt
@@ -0,0 +1,26 @@
+package io.deepmatter.pokemon.core
+
+import io.deepmatter.pokemon.api.adapter.COMMON
+import io.deepmatter.pokemon.model.Card
+import io.deepmatter.pokemon.model.Rarity
+import io.deepmatter.pokemon.util.random.Randomiser
+import io.deepmatter.pokemon.viewmodel.Round
+
+class RoundFactoryImpl(private val randomiser: Randomiser) : RoundFactory {
+
+    override fun buildRound(cards: List<Card>): Round {
+        val cards1 = cards.distinctBy { it.rarity }.take(2)
+        val rareCard = cards1.find { it.rarity == Rarity.Rare }
+        val uncommonCard=cards1.find { it.rarity == Rarity.Uncommon }
+        val commonCard = cards1.find { it.rarity == Rarity.Common }
+
+        if(rareCard == null){
+            return Round(cards1, uncommonCard!!)
+        }
+        else  if (uncommonCard ==null) {
+        return Round(cards1, commonCard!!)
+    }
+        else {return Round(cards1, rareCard)
+}
+    }
+}
